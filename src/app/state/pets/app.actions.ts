039b6033a4adf40162bf61d4209c06c2cf3fb57a
luzajic91 <vladan.luzajic@gmail.com>
2023-10-21 11:04:21 +0200

    reducer posalice

--- src/app/state/pets/app.actions.ts
+++ src/app/state/pets/app.actions.ts
@@ -3,5 +3,6 @@ import { Pet } from "src/app/models/pet";
 
 export const addPet = createAction('[Pet] Add', props<{ pet: Pet }>());
 export const updatePet = createAction('[Pet] Update', props<{ pet: Pet }>());
-export const deletePet = createAction('[Pet] Delete', props<{ Id: number }>());
-export const loadPets = createAction('[Pet] Load', props <{pets: Pet[]}>());
+export const deletePet = createAction('[Pet] Delete', props<{ id: number }>());
+export const loadPets = createAction('[Pet] Load');
+export const loadPetsSuccess = createAction('[Pet] Load', props <{pets: Pet[]}>());
